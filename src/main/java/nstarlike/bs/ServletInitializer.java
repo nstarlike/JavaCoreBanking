0da034862390c31231a3442c86e03fe40c0520a6
nstarlike <46280877+nstarlike@users.noreply.github.com>
2023-05-25 13:15:37 +0900

    Add Spring Boot project

---
+++ src/main/java/nstarlike/bs/ServletInitializer.java
@@ -0,0 +1,13 @@
+package nstarlike.bs;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BankSystemApplication.class);
+	}
+
+}
